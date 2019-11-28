68746b1105377f2f5cb259f967085da6c950bfa6
liaohao <1094370130@qq.com>
2019-11-28 23:04:19 +0800

    使用generate生成film数据库相关文件

--- guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MtimeCatDictTMapper.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MtimeCatDictTMapper.java
@@ -1,16 +1,16 @@
 package com.stylefeng.guns.rest.common.persistence.dao;
 
-import com.stylefeng.guns.rest.common.persistence.model.MtimeFilmT;
+import com.stylefeng.guns.rest.common.persistence.model.MtimeCatDictT;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 
 /**
  * <p>
- * 影片主表 Mapper 接口
+ * 类型信息表 Mapper 接口
  * </p>
  *
  * @author liaohao
  * @since 2019-11-28
  */
-public interface MtimeFilmTMapper extends BaseMapper<MtimeFilmT> {
+public interface MtimeCatDictTMapper extends BaseMapper<MtimeCatDictT> {
 
 }
--- guns-film/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
+++ guns-film/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
@@ -29,13 +29,13 @@ public class EntityGenerator {
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("D:\\Program_Software\\IntelliJ_IDEA\\GunsProjects\\personalCinema\\guns\\guns-film\\src\\main\\java");//这里写你自己的java目录
+        gc.setOutputDir("D:\\me_Workspaces\\3rdObject\\CinemaProject\\guns-film\\src\\main\\java");//这里写你自己的java目录
         gc.setFileOverride(true);//是否覆盖
         gc.setActiveRecord(true);
         gc.setEnableCache(false);// XML 二级缓存
         gc.setBaseResultMap(true);// XML ResultMap
         gc.setBaseColumnList(false);// XML columList
-        gc.setAuthor("fwj");
+        gc.setAuthor("liaohao");
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
@@ -51,14 +51,22 @@ public class EntityGenerator {
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("123456");
-        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/guns_rest?autoReconnect=true&useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8");
+        dsc.setUrl("jdbc:mysql://192.168.48.5:3306/guns_rest?autoReconnect=true&useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8");
         mpg.setDataSource(dsc);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
         //strategy.setTablePrefix(new String[]{"_"});// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        strategy.setInclude(new String[]{"mtime_film_t"});
+        strategy.setInclude(new String[]{
+                "mtime_actor_t",
+                "mtime_banner_t",
+                "mtime_cat_dict_t",
+                "mtime_film_actor_t",
+                "mtime_film_info_t",
+                "mtime_film_t",
+                "mtime_source_dict_t",
+                "mtime_year_dict_t"});
         mpg.setStrategy(strategy);
 
         // 包配置
